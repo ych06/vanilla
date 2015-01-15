b8706e5274c07d4550da980d78bf16e79fe6d299
Todd Burry <todd@vanillaforums.com>
2015-01-15 10:47:03 -0500

    Harden registrations.
    This is a cherry-pick from an upstream change.

--- applications/dashboard/controllers/class.entrycontroller.php
+++ applications/dashboard/controllers/class.entrycontroller.php
@@ -572,7 +572,7 @@ class EntryController extends Gdn_Controller {
          if ($this->Form->GetFormValue('Name') && $EmailValid && (!is_array($ExistingUsers) || count($ExistingUsers) == 0)) {
             // There is no existing user with the suggested name so we can just create the user.
             $User = $this->Form->FormValues();
-            $User = $this->UserModel->FilterForm($User);
+            $User = $this->UserModel->FilterForm($User, TRUE);
             $User['Password'] = RandomString(50); // some password is required
             $User['HashMethod'] = 'Random';
             $User['Source'] = $this->Form->GetFormValue('Provider');
@@ -670,7 +670,7 @@ class EntryController extends Gdn_Controller {
          } elseif ($this->Form->ErrorCount() == 0) {
             // The user doesn't exist so we need to add another user.
             $User = $this->Form->FormValues();
-            $User = $this->UserModel->FilterForm($User);
+            $User = $this->UserModel->FilterForm($User, TRUE);
             $User['Name'] = $User['ConnectName'];
             $User['Password'] = RandomString(50); // some password is required
             $User['HashMethod'] = 'Random';
@@ -1233,7 +1233,7 @@ class EntryController extends Gdn_Controller {
          
          try {
             $Values = $this->Form->FormValues();
-            $Values = $this->UserModel->FilterForm($Values);
+            $Values = $this->UserModel->FilterForm($Values, TRUE);
             unset($Values['Roles']);
             $AuthUserID = $this->UserModel->Register($Values);
             if (!$AuthUserID) {
@@ -1293,7 +1293,7 @@ class EntryController extends Gdn_Controller {
          
          try {
             $Values = $this->Form->FormValues();
-            $Values = $this->UserModel->FilterForm($Values);
+            $Values = $this->UserModel->FilterForm($Values, TRUE);
             unset($Values['Roles']);
             $AuthUserID = $this->UserModel->Register($Values);
             if ($AuthUserID == UserModel::REDIRECT_APPROVE) {
@@ -1365,7 +1365,7 @@ class EntryController extends Gdn_Controller {
          
          try {
             $Values = $this->Form->FormValues();
-            $Values = $this->UserModel->FilterForm($Values);
+            $Values = $this->UserModel->FilterForm($Values, TRUE);
             unset($Values['Roles']);
             $AuthUserID = $this->UserModel->Register($Values);
             if ($AuthUserID == UserModel::REDIRECT_APPROVE) {
@@ -1441,7 +1441,7 @@ class EntryController extends Gdn_Controller {
          
          try {
             $Values = $this->Form->FormValues();
-            $Values = $this->UserModel->FilterForm($Values);
+            $Values = $this->UserModel->FilterForm($Values, TRUE);
             unset($Values['Roles']);
             $AuthUserID = $this->UserModel->Register($Values);
 
